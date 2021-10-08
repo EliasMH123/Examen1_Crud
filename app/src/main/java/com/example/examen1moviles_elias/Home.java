5c2696b6a8ce56ad8e8ab51092e402f6d0bb6a15
EliasMH123 <elias.mamani@upeu.edu.pe>
2021-10-07 20:22:16 -0500

    logout

--- app/src/main/java/com/example/examen1moviles_elias/Home.java
+++ app/src/main/java/com/example/examen1moviles_elias/Home.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 
 public class Home extends AppCompatActivity {
@@ -19,5 +21,24 @@ public class Home extends AppCompatActivity {
             finish();
         });
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //action_menu_salirselect
+        switch (id){
+            case R.id.item_salir:
+                finish();
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
